d8499def83f8dad700e9fc84274c5c974ff93ee0
LordSaruman <filip.ivanovic.wp@gmail.com>
2018-05-18 21:37:59 +0200

    0.1

--- src/clojure_webapp/core.clj
+++ src/clojure_webapp/core.clj
@@ -1,6 +1,61 @@
-(ns clojure-webapp.core)
+(ns clojure-webapp.core
+  (:use compojure.core)
+  (:require [compojure.handler :as handler]
+            [compojure.route :as route]
+            [ring.middleware.basic-authentication :refer :all]
+            [ring.util.response :as resp]
+            [clojure-webapp.webController.controller :as controller]
+            [clojure-webapp.domain.bank :as bank-domain]
+            [clojure-webapp.domain.euribor :as euribor-domain]
+            [clojure-webapp.domain.interest :as interest-domain]
+            [clojure-webapp.domain.typeinterest :as typeinterest-domain]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+(defroutes public-routes
+           (GET "/" [] (controller/index))
+           (route/resources "/")
+           (GET "/index" [] (controller/index))
+           (route/resources "/")
+           (GET "/banks" [] (controller/banks))
+           (route/resources "/")
+           (GET "/interests" [] (controller/interests))
+           (route/resources "/")
+
+           (GET "/model/banks/:id/remove" [id]
+             (do (bank-domain/removeBank id)
+                 (resp/redirect "/banks")))
+
+           (GET "/model/interests/:id/remove" [id]
+             (do (interest-domain/removeInterest id)
+                 (resp/redirect "/interests")))
+
+           (POST "/model/banks/insert" [& params]
+             (do (bank-domain/insertBank params)
+                 (resp/redirect "/banks")))
+
+           (POST "/model/interests/insert" [& params]
+             (do (interest-domain/insertInterest params)
+                 (resp/redirect "/interests")))
+
+           (GET "/model/banks/:id/update" [id] (controller/updatingBanks id))
+
+           (POST "/model/banks/:id_bank/update" [& params]
+             (do (bank-domain/update (:id_bank params) params)
+                 (resp/redirect "/banks")))
+
+           (GET "/model/interests/:id/update" [id] (controller/updatingInterests id))
+
+           (POST "/model/interests/:id_interest/update" [& params]
+             (do (interest-domain/update (:id_interest params) params)
+                 (resp/redirect "/interests")))
+
+           )
+
+
+
+(defroutes app-routes
+           public-routes
+           (route/not-found "404 Not Found"))
+
+(def -main
+  (handler/site app-routes))
